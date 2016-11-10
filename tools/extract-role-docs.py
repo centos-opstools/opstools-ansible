816761d5e6f148a3a29e8c8645af5a3eec95db35
Juan Badia Payno <jbadiapa@redhat.com>
2016-11-10 13:16:53 +0100

    Modified documentator generator
    Change-Id: I473d50b12b34c1f5f6bba85e50dc6bb6b1872d2e

--- tools/extract-role-docs.py
+++ tools/extract-role-docs.py
@@ -8,7 +8,7 @@ import sys
 import yaml
 
 
-def generate_docs(roles, output=sys.stdout):
+def generate_docs(roles, playbook, output=sys.stdout):
     for dirpath, dirnames, filenames in os.walk(roles):
         for keydir in ['meta', 'tasks', 'defaults']:
             if keydir in dirnames:
@@ -18,10 +18,39 @@ def generate_docs(roles, output=sys.stdout):
 
         README = os.path.join(dirpath, 'README.md')
         DEFAULTS = os.path.join(dirpath, 'defaults', 'main.yml')
+        TASKS = os.path.join(dirpath, 'tasks', 'main.yml')
 
         if os.path.isfile(README):
             with open(README, 'r') as fd:
                 output.write(fd.read())
+
+        if os.path.isfile(TASKS):
+            output.write('\n')
+            output.write('### Actions defined on the role')
+            output.write('\n\n')
+
+            with open(TASKS, 'r') as fd:
+                data = yaml.load(fd)
+
+            for task in data:
+                if 'name' in task.keys():
+                    output.write('> - ')
+                    output.write(task['name'])
+                    output.write('\n')
+                if 'include' in task.keys():
+                    FILE = os.path.join(dirpath, 'tasks', task['include'])
+                    with open(FILE, 'r') as fd:
+                        included_data = yaml.load(fd)
+                    output.write('\n> ')
+                    output.write(task['include'])
+                    output.write('\n\n')
+                    for inc_task in included_data:
+                        if 'name' in inc_task.keys():
+                            output.write('> - ')
+                            output.write(inc_task['name'])
+                            output.write('\n')
+
+            output.write('\n')
 
         if os.path.isfile(DEFAULTS):
             output.write('\n')
@@ -57,14 +86,24 @@ def parse_args():
     p = argparse.ArgumentParser()
     p.add_argument('--roles', '-r', default='roles')
     p.add_argument('--output', '-o', default='-')
+    p.add_argument('--playbook', '-p', default='playbook.yml')
     return p.parse_args()
+
+
+def validate_args(args):
+    if args.playbook and (not os.path.isfile(args.playbook)):
+        print("The playbook can not be read")
+        sys.exit(1)
 
 
 def main():
     args = parse_args()
 
+    validate_args(args)
+
+    pb = []
     with sys.stdout if args.output == '-' else open(args.output, 'w') as fd:
-        generate_docs(args.roles, output=fd)
+        generate_docs(args.roles, pb, output=fd)
 
 
 if __name__ == '__main__':
